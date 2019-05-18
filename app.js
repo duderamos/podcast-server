e31859373e7ba59df1061561d2c0eff8e84c28f7
Eduardo de Lima Ramos <ramos.eduardo87@gmail.com>
2019-05-18 22:29:34 +1000

    Remove unused code

--- app.js
+++ app.js
@@ -9,9 +9,6 @@ var cors = require('cors');
 var Podcast = require('./models/Podcast');
 var Episode = require('./models/Episode');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 mongoose.connect('mongodb://localhost/podcast', { promiseLibrary: require('bluebird'), useNewUrlParser: true })
         .then(() => console.log('connection successful'))
         .catch((err) => console.error(err));
@@ -24,8 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('*', cors());
-
-app.use('/', indexRouter);
 
 app.get('/populate', async (req, res) => {
   let Parser = require('rss-parser');
